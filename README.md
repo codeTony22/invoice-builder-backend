d3df92dbd43931411b5198c33544ce323432de91
codeTony22 <jgonz275@fiu.edu>
2018-12-27 23:07:39 -0500

    Eslint and prettier integration.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # invoice-builder-backend
+
+##Eslint 
+npm install -g eslint@4.19.1
--- src/app.js
+++ src/app.js
@@ -5,6 +5,17 @@ const express = require('express');
 //Start express app
 const app = express();
 const PORT = 3000;
+
+//Routes
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'Welcome to Invoice builder backend'
+    })
+});
+
+//Middlewares
+
+
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
 });
